e7c7086f6626797ffb7abd68945f6085d4729a7b
LENOVO <ekarifanurulhsanah@gmail.com>
2026-05-17 16:25:55 +0700

    putri_

---
+++ lib/services/eka_komservices.dart
@@ -0,0 +1,34 @@
+import 'package:chtera/model/eka_kommodel.dart';
+
+List<EkaKommodel> komm = [
+  EkaKommodel(
+    judul: 'Stiker lucu',
+    sub: 'janlup follow ya',
+    url: 'asset/st.jpeg',
+  ),
+  EkaKommodel(
+    judul: 'Lagu jawa',
+    sub: 'piee rek',
+    url: 'asset/st.jpeg',
+  ),
+  EkaKommodel(
+    judul: 'ramadhan',
+    sub: 'selamat hari raya ',
+    url: 'asset/st.jpeg',
+  ),
+  EkaKommodel(
+    judul: 'lirik lagu',
+    sub: 'Semangat selalu',
+    url: 'asset/st.jpeg',
+  ),
+  EkaKommodel(
+    judul: 'Stiker lucu',
+    sub: 'janlup follow ya',
+    url: 'asset/st.jpeg',
+  ),
+  EkaKommodel(
+    judul: 'Stiker lucu',
+    sub: 'janlup follow ya',
+    url: 'asset/st.jpeg',
+  ),
+];
