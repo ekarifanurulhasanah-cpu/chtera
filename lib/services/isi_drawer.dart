7e198c0d2e32d600f251082dda516e1868c01b4e
LENOVO <ekarifanurulhsanah@gmail.com>
2026-05-21 17:13:26 +0700

    isi_c

---
+++ lib/services/isi_drawer.dart
@@ -0,0 +1,7 @@
+import 'package:chtera/model/isid_rawer.dart';
+import 'package:flutter/material.dart';
+
+List<IsidRawer> wer = [
+  IsidRawer(icon: Icons.settings, title: "Settings"),
+  IsidRawer(icon: Icons.privacy_tip, title: "Privasy")
+];
