ebd839d067b38111c9aae74d3bc08dc71974600a
LENOVO <ekarifanurulhsanah@gmail.com>
2026-05-21 16:59:51 +0700

    isi_c

---
+++ lib/services/isihmepro_services.dart
@@ -0,0 +1,10 @@
+import 'package:chtera/model/isihmepro_model.dart';
+
+List<IsihmeproModel> ishp= [
+  IsihmeproModel(url: 'ot.jpeg'),
+  IsihmeproModel(url: 'ot.jpeg'),
+  IsihmeproModel(url: 'ot.jpeg'),
+  IsihmeproModel(url: 'ot.jpeg'),
+  IsihmeproModel(url: 'ot.jpeg'),
+  IsihmeproModel(url: 'ot.jpeg'),
+];
